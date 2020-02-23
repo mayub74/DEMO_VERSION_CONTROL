1fa99f5accc2c6d9d8ac1374dd954b84e67018a7
mayub74 <mayuriborate1997@gmail.com>
2020-02-23 20:25:21 +0530

    adding files

---
+++ VERSION_CONTROL/mobile_validate.py
@@ -0,0 +1,16 @@
+#python code to validate mobile no using regular expression.
+import re
+def isValid(s):    
+    # 1) Begins with 0 or 91 
+    # 2) Then contains 7 or 8 or 9. 
+    # 3) Then contains 9 digits 
+    Pattern = re.compile("^(0/91)?[7-9][0-9]{9}$") 
+    return Pattern.match(s) 
+  
+# Driver Code 
+s =  input("enter mobile no ") 
+if (isValid(s)):  
+    print ("Valid Number")      
+else : 
+    print ("Invalid Number")   
+  
